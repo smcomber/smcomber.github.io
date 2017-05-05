c0b82114d8b7d6dbd605e14748d26221a831bdb9
smcomber <sethmcomber@gmail.com>
2017-05-04 20:14:24 -0700

    completed states

--- workout.js
+++ workout.js
@@ -1,53 +1,73 @@
-var complete = [];
+var wkComplete = [];
+var woComplete = [];
+var wkCompleteString;
+var woCompleteString;
 
-function setComplete(){
-$('h2').click(function(){
+
+$('h2').click(function () {
 	var thisClick = $(this);
 	var thisWeek = $(thisClick).parent().attr('id');
-	if ($(thisClick).parent().hasClass('closed')){
-		$(thisClick).parent().removeClass('closed');
-		var index = complete.indexOf(thisWeek);
-		if (index > -1) {
-			complete.splice(index, 1);
-		}
+	// check to see if item is already set as complete
+	var index = wkComplete.indexOf(thisWeek);
+	if (index > -1) {
+		wkComplete.splice(index, 1);
+		wkCompleteString = JSON.stringify(wkComplete);
+		localStorage.setItem('wkComplete', wkCompleteString);
+		$(thisClick).parent().removeClass('wkClosed');
+	} else {
+		wkComplete.push(thisWeek);
+		wkCompleteString = JSON.stringify(wkComplete);
+		localStorage.setItem('wkComplete', wkCompleteString);
+		$(thisClick).parent().addClass('wkClosed');
 	}
-	else {
-		$(thisClick).parent().addClass('closed');
-		complete.push(thisWeek);
-	localStorage.setItem('complete', complete);
-	}
-
+	//console.log(complete);
 })
-}
 
-function checkComplete(){
-//localStorage.removeItem('complete');
-	var storedComplete = localStorage.getItem('complete');
-	if (storedComplete != null){
-		storedComplete = storedComplete.split(',');
-		for (i=0; i < storedComplete.length; i++){
-			checkWeek(storedComplete[i])
-			complete.push(storedComplete[i]);
-	
+$('.woComplete').click(function () {
+	var thisClick = $(this);
+	var thisWorkout = $(thisClick).parent().attr('class');
+	thisWorkout = thisWorkout.split(' ');
+	thisWorkout = thisWorkout[1];
+	// check to see if item is already set as complete
+
+	var index = woComplete.indexOf(thisWorkout);
+	if (index > -1) {
+		woComplete.splice(index, 1);
+		woCompleteString = JSON.stringify(woComplete);
+		localStorage.setItem('woComplete', woCompleteString);
+		$(thisClick).parent().removeClass('woClosed');
+	} else {
+		woComplete.push(thisWorkout);
+		woCompleteString = JSON.stringify(woComplete);
+		localStorage.setItem('woComplete', woCompleteString);
+		$(thisClick).parent().addClass('woClosed');
+	}
+
+	console.log(woComplete);
+})
+
+// get stored completion record and set styles - week
+function checkComplete() {
+	//localStorage.removeItem('wkComplete');
+	var storedComplete = localStorage.getItem('wkComplete');
+	storedComplete = JSON.parse(storedComplete);
+	if (storedComplete != null) {
+		for (i = 0; i < storedComplete.length; i++) {
+			var weekId = storedComplete[i]
+			var week = $('.week');
+			for (v = 0; v < week.length; v++) {
+				var thisWeekId = $(week[v]).attr('id');
+				if (thisWeekId == weekId) {
+					$(week[v]).addClass('wkClosed');
+				}
+			}
+			wkComplete.push(storedComplete[i]);
 		}
 	}
-console.log(complete);
-setComplete();
+	//console.log(wkComplete);
+	//woCheckComplete();
 }
 
-function checkWeek(weekId){
-	var week = $('.week');
-	for (v=0; v < week.length; v++){
-		var thisWeekID = $(week[v]).attr('id');
-		if (thisWeekID == weekId){
-			$(week[v]).addClass('closed');
-			return;
-			}
-		else {
-			return;
-		}
 
-}	
-} 
+
 window.onload = checkComplete;
-
